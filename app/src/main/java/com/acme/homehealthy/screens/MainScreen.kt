a6b6ef4a52f05dd6247b0864f7518bbc42f0f595
SebTou08 <sebaas14toulier@gmail.com>
2021-09-17 12:20:35 -0500

    feat: add routine row

--- app/src/main/java/com/acme/homehealthy/screens/MainScreen.kt
+++ app/src/main/java/com/acme/homehealthy/screens/MainScreen.kt
@@ -2,12 +2,16 @@ package com.acme.homehealthy.screens
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import com.acme.homehealthy.data.models.Routine
 import com.acme.homehealthy.ui.theme.DeepBlue
 import com.acme.homehealthy.ui.theme.Rose
 
@@ -48,3 +52,28 @@ fun Greetings(name: String){
 }
 
 
+@Composable
+fun routinesList(routines: List<Routine>){
+    LazyColumn(){
+        items(routines){
+            routine -> RoutineRow(routine)
+        }
+    }
+}
+
+@Composable
+fun RoutineRow(routine: Routine){
+    Card (
+        modifier = Modifier
+            .padding(5.dp)
+            .fillMaxWidth(),
+        elevation = 5.dp
+            ){
+        Row (
+            modifier = Modifier
+                .padding(4.dp)
+                ){
+        }
+
+    }
+}
