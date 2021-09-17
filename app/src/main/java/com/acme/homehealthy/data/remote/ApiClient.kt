33a03b28c757861136e514c9fff3d11d21cdc56a
SebTou08 <sebaas14toulier@gmail.com>
2021-09-17 11:29:06 -0500

    feat: add builds funs in api client

--- app/src/main/java/com/acme/homehealthy/data/remote/ApiClient.kt
+++ app/src/main/java/com/acme/homehealthy/data/remote/ApiClient.kt
@@ -1,6 +1,35 @@
 package com.acme.homehealthy.data.remote
 
+import com.acme.homehealthy.data.remote.interfaces.RoutineInterface
+import com.acme.homehealthy.data.remote.interfaces.TrainingInterface
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 object ApiClient {
     private const val API_BASE_URL = "http://localhost:8095"
+
+    //Routines
+    var routineInterface : RoutineInterface? = null
+
+    fun buildRoutine() : RoutineInterface? {
+        val builder: Retrofit.Builder = Retrofit.Builder()
+            .baseUrl(API_BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+        var retrofit: Retrofit = builder.build()
+        routineInterface = retrofit.create(RoutineInterface::class.java)
+        return routineInterface as RoutineInterface
+    }
+
+    //Trainings
+    var trainingInterface : TrainingInterface? = null
+
+    fun buildTraining(): TrainingInterface?{
+        val builder: Retrofit.Builder = Retrofit.Builder()
+            .baseUrl(API_BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+        var retrofit: Retrofit = builder.build()
+        trainingInterface = retrofit.create(TrainingInterface::class.java)
+        return trainingInterface as TrainingInterface
+    }
 
 }
