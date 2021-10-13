5f788dcb9d019f01c812cd20562dc99c79a4ae3c
SebTou <sebaas14toulier@gmail.com>
2021-10-13 16:48:11 -0500

    feat: add diet interface

--- app/src/main/java/com/acme/homehealthy/data/remote/ApiClient.kt
+++ app/src/main/java/com/acme/homehealthy/data/remote/ApiClient.kt
@@ -1,5 +1,6 @@
 package com.acme.homehealthy.data.remote
 
+import com.acme.homehealthy.data.remote.interfaces.DietInterface
 import com.acme.homehealthy.data.remote.interfaces.RoutineInterface
 import com.acme.homehealthy.data.remote.interfaces.TrainingInterface
 import retrofit2.Retrofit
@@ -7,6 +8,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object ApiClient {
     private const val API_BASE_URL = "https://60c255b2069afc0017f4a2ca.mockapi.io/"
+    private const val API_BASE_URL_DIET = "https://60c2e09f917002001739da47.mockapi.io/"
 
     //Routines
     var routineInterface: RoutineInterface? = null
@@ -31,5 +33,17 @@ object ApiClient {
         trainingInterface = retrofit.create(TrainingInterface::class.java)
         return trainingInterface as TrainingInterface
     }
+
+    //Diet
+    var dietInterface: DietInterface?=null
+    fun buildDiets(): DietInterface?{
+        val builder: Retrofit.Builder = Retrofit.Builder()
+            .baseUrl(API_BASE_URL_DIET)
+            .addConverterFactory(GsonConverterFactory.create())
+        var retrofit: Retrofit = builder.build()
+        dietInterface = retrofit.create(DietInterface::class.java)
+        return dietInterface as DietInterface
+    }
+
 
 }
