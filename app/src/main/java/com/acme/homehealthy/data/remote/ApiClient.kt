fc22fdb3e2e358ea87c75fd2a8858729c4ac51a7
Sebastian <sebaas14toulier@gmail.com>
2021-11-25 16:23:25 -0500

    feat: add routine detail interfae and model

--- app/src/main/java/com/acme/homehealthy/data/remote/ApiClient.kt
+++ app/src/main/java/com/acme/homehealthy/data/remote/ApiClient.kt
@@ -1,9 +1,6 @@
 package com.acme.homehealthy.data.remote
 
-import com.acme.homehealthy.data.remote.interfaces.DietInterface
-import com.acme.homehealthy.data.remote.interfaces.RoutineInterface
-import com.acme.homehealthy.data.remote.interfaces.TrainingInterface
-import com.acme.homehealthy.data.remote.interfaces.UserInterface
+import com.acme.homehealthy.data.remote.interfaces.*
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -11,6 +8,7 @@ object ApiClient {
     private const val API_BASE_URL = "https://60c255b2069afc0017f4a2ca.mockapi.io/"
     private const val API_BASE_URL_DIET = "https://60c2e09f917002001739da47.mockapi.io/"
     private const val API_BASE_URL_USER = "https://60c2e09f917002001739da47.mockapi.io/"
+
 
     //Routines
     var routineInterface: RoutineInterface? = null
@@ -56,6 +54,12 @@ object ApiClient {
         var retrofit: Retrofit = builder.build()
         userInterface = retrofit.create(UserInterface::class.java)
         return userInterface as UserInterface
+    }
+
+    //RoutineDetail
+    var routineDetailInterface:RoutineInterface? = null
+    fun buildRoutineDetail(): RoutineDetailInterface?{
+        val builder: Retrofit.Builder = Retrofit.Builder()
     }
 
 
